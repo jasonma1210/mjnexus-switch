b336683d02df7f72c8280f0e6ae351adbc7d9d30
jasonma1210 <jason_ma1982@qq.com>
2026-08-28 20:36:22 +0800

    fix HomePage.cpp: hardcoded colors for now

--- source/app/HomePage.cpp
+++ source/app/HomePage.cpp
@@ -4,7 +4,7 @@ namespace mjnexus
 {
     HomePage::HomePage() : brls::Box()
     {
-        // 首页最小骨架
+        brls::Logger::info("HomePage created");
     }
 
     void HomePage::draw(NVGcontext* vg, float x, float y, float width, float height,
@@ -12,25 +12,25 @@ namespace mjnexus
     {
         brls::Box::draw(vg, x, y, width, height, style, ctx);
 
-        // 临时占位：画个背景色
-        NVGcolor bg = brls::Application::getTheme()
-            ? brls::Application::getTheme()->getColor("bgColor")
-            : nvgRGBA(20, 20, 20, 255);
-
+        // 背景（固定深色，避免 getTheme 问题）
         nvgBeginPath(vg);
         nvgRect(vg, x, y, width, height);
-        nvgFillColor(vg, bg);
+        nvgFillColor(vg, nvgRGBA(20, 20, 20, 255));
         nvgFill(vg);
 
-        // 临时标题
+        // 标题
         nvgFillColor(vg, nvgRGBA(255, 255, 255, 255));
-        nvgFontSize(vg, 40);
+        nvgFontSize(vg, 48);
         nvgFontFace(vg, "standard");
         nvgTextAlign(vg, NVG_ALIGN_CENTER | NVG_ALIGN_MIDDLE);
-        nvgText(vg, x + width / 2, y + height / 2 - 40, "MJ Reader", nullptr);
+        nvgText(vg, x + width / 2, y + height / 2 - 60, "MJ Reader", nullptr);
 
-        nvgFontSize(vg, 22);
-        nvgText(vg, x + width / 2, y + height / 2 + 10, "Borealis moonlight_wiliwili", nullptr);
-        nvgText(vg, x + width / 2, y + height / 2 + 40, "Press A to continue...", nullptr);
+        nvgFontSize(vg, 20);
+        nvgFillColor(vg, nvgRGBA(180, 180, 180, 255));
+        nvgText(vg, x + width / 2, y + height / 2 - 15, "Nintendo Switch Homebrew", nullptr);
+        nvgText(vg, x + width / 2, y + height / 2 + 20, "Borealis moonlight_wiliwili", nullptr);
+        nvgText(vg, x + width / 2, y + height / 2 + 55, "Build skeleton - v2", nullptr);
+
+        brls::Logger::info("HomePage draw at {:.0f}x{:.0f}", width, height);
     }
 }
