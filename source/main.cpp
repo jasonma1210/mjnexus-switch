a3e59fe6e89d448f00ff030bac2ccc140fd9653c
jasonma1210 <jason_ma1982@qq.com>
2026-08-28 20:36:18 +0800

    fix main.cpp: Application::exit

--- source/main.cpp
+++ source/main.cpp
@@ -1,35 +1,25 @@
 #include <switch.h>
 #include <borealis.hpp>
 #include <cstdio>
-#include <string>
-
-namespace mjnexus
-{
-    class HomeActivity; // 前向声明
-}
 
 int main(int argc, char* argv[])
 {
-    // 初始化 Borealis（无参数）
     if (!brls::Application::init())
     {
         brls::Logger::error("Borealis init failed!");
         return -1;
     }
 
-    // 创建窗口
     brls::Application::createWindow("MJ Reader");
 
-    // 创建首页 Activity（传入 HomePage View）
-    // 注意：Activity 构造需要 View*，稍后创建
-    // brls::Application::pushActivity(new mjnexus::HomeActivity());
+    // TODO: push HomeActivity
+    // brls::Application::pushActivity(new HomeActivity());
 
-    // 主循环
     while (brls::Application::mainLoop())
     {
-        // 可以在此处理额外逻辑
+        // 空循环，Activity 管理 UI
     }
 
-    brls::Application::getPlatform()->exit();
+    brls::Application::exit();
     return 0;
 }
